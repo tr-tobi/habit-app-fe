0596d63c543af04e45ee77a7383354dc4b39758f
George Samuels <dominic.g.samuels@outlook.com>
2023-09-19 11:59:26 +0100

    signup form

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
+import SignupForm from "./components/SignUpForm";
 
 export default function App() {
+  const handleSignup = (username: string, email: string, password: string) => {
+    console.log("Signing up with:", username, email, password);
+  };
+
+  const checkUniqueUsername = (username: string) => {
+    return true;
+  };
   return (
     <View style={styles.container}>
-      <Header title="Habit Tracker!"/> 
+      <Header title="Habit Tracker!" />
+      <SignupForm
+        onSignup={handleSignup}
+        checkUniqueUsername={checkUniqueUsername}
+      />
     </View>
   );
 }
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
