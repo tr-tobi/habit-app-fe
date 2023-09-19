c5b0a382000bd22f18df9dbb883b8ff99f316905
George Samuels <dominic.g.samuels@outlook.com>
2023-09-19 17:28:32 +0100

    started to add the screen for the signup page

---
+++ screens/SignUpPage.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "../components/Header";
+import SignupForm from "../components/SignUpForm";
+import SignInForm from "../components/SignInForm";
+
+export default function SignUpPage() {
+  const handleSignup = (username: string, email: string, password: string) => {
+    console.log("Signing up with:", username, email, password);
+  };
+
+  const handleSignIn = (username: string) => {
+    console.log("Signing in with:", username);
+  };
+
+  const checkUniqueUsername = (username: string) => {
+    return true;
+  };
+  return (
+    <View style={styles.container}>
+      <Header title="Habit Tracker!" />
+      <Text>Create an Account!</Text>
+      <SignupForm
+        onSignup={handleSignup}
+        checkUniqueUsername={checkUniqueUsername}
+      />
+      <Text>Sign In!</Text>
+      <SignInForm onSignIn={handleSignIn} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
