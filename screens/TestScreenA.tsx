75d99d3ed60208d2c7072ae11b656df33a7c16d4
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-09-19 15:06:50 +0100

    adds example test screens

---
+++ screens/TestScreenA.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function TestScreenA() {
+    return (
+        <View>
+            <Text>This is Test Screen A</Text>
+            <Text>This is for demonstration purposes</Text>
+            <Text>Delete when no longer required</Text>
+        </View>
+    )
+}
