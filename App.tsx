8fb3dfbb787207971d5484ed9caef333f2e9790f
usmosis <usman_ali7@outlook.com>
2023-09-19 16:01:40 +0100

    adds list of habits with checkboxes

--- App.tsx
+++ App.tsx
@@ -3,14 +3,21 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import Notes from './components/Notes';
 import HabitsList from './components/HabitsList';
+import { useState } from 'react';
 
 export default function App() {
+const [habits, setHabits] = useState([
+  { id: 1, name: 'Exercise', description: '20 minutes of movement', completed: false },
+  { id: 2, name: 'Read', description: 'minimum 3 pages', completed: false },
+  { id: 3, name: 'Meditate', description: 'minimum 5 minutes', completed: false }]
+)
+
   return (
     
     <View style={styles.container}>
       <Header title="Habit Tracker!"/> 
       <Notes body = {""}/>
-      <HabitsList/>
+      <HabitsList habits={habits} setHabits={setHabits}/>
 
     </View>
    
