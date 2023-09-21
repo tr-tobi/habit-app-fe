4537895328975b253e21cedaec33cbdf03fdd593
George Samuels <dominic.g.samuels@outlook.com>
2023-09-21 10:39:21 +0100

    adding password hash with bcrypt

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -10,22 +10,21 @@ function SignInForm({ onSignIn }: SignInFormProps) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSignIn = () => {
-    onSignIn(username)
-    // postSignIn(username, password)
-    //   .then((response) => {
-    //     if (response.data.success) {
-    //       onSignIn(username);
-    //     } else {
-    //       Alert.alert(
-    //         "Invalid credentials",
-    //         "Please check your username and password."
-    //       );
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error signing in:", error);
-    //   });
+  const handleSignIn = async () => {
+    try {
+      const response = await postSignIn(username, password);
+
+      if (response.data.success) {
+        onSignIn(username);
+      } else {
+        Alert.alert(
+          "Invalid credentials",
+          "Please check your username and password."
+        );
+      }
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
   };
 
   return (
