5f7912083b72d50a711c73bdbf60128d8ab69a04
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-09-22 11:39:44 +0100

    adds delete habit dialog

---
+++ components/DeleteHabitDialog.tsx
@@ -0,0 +1,30 @@
+import { Button, Dialog, Portal, Text } from 'react-native-paper';
+
+interface DeleteHabitProps {
+    visible: boolean
+    hideDialog: () => void
+    deleteHabit: () => void
+}
+
+export default function DeleteHabitDialog({visible, hideDialog, deleteHabit}: DeleteHabitProps) {
+    function handleDelete() {
+        deleteHabit()
+        hideDialog()
+    }
+    
+    
+    return (
+        <Portal>
+          <Dialog visible={visible} onDismiss={hideDialog}>
+            <Dialog.Title>Delete Habit</Dialog.Title>
+            <Dialog.Content>
+              <Text variant="bodyMedium">All data about this habit will be lost</Text>
+            </Dialog.Content>
+            <Dialog.Actions>
+            <Button onPress={hideDialog}>Cancel</Button>
+              <Button onPress={handleDelete}>Delete</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
+    )
+}
