13cdeddf25576997b23025ff3004c96b6e0d655a
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-09-23 14:23:22 +0100

    Adds index file for screens

--- App.tsx
+++ App.tsx
@@ -2,9 +2,7 @@ import { PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NavHeader from "./components/NavHeader";
-import HomeScreen from "./screens/HomeScreen";
-import SignUpPage from "./screens/SignUpPage";
-import CalendarHabitScreen from "./screens/CalendarHabitScreen";
+import { HomeScreen, SignUpPage, CalendarHabitScreen } from "./screens";
 import { HabitsProvider } from "./contexts/Habits";
 import { HabitCompletionProvider } from "./contexts/HabitCompletion";
 
@@ -17,7 +15,7 @@ export default function App() {
         <HabitsProvider>
           <HabitCompletionProvider>
             <Stack.Navigator initialRouteName="SignUp/SignIn" screenOptions={{ header: NavHeader }}>
-              
+
               <Stack.Screen name="SignUp/SignIn" options={{ title: "SignUp/SignIn", headerShown: false }}>
                 {(props) => <SignUpPage />}
               </Stack.Screen>
