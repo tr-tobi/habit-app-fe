86586464ff04609e2c5371348809e02f6afe278e
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-09-28 10:35:28 +0100

    adds api call

--- components/HabitsList.tsx
+++ components/HabitsList.tsx
@@ -1,9 +1,11 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { FlatList, View } from "react-native";
 import HabitCard from "./HabitCard";
 import { HabitsContext } from "../contexts/Habits";
 import { HabitSetter, HabitsContextType } from "../types";
 import { Button } from "react-native-paper";
+import { getHabits } from "../requests/Requests";
+import { useUserContext } from "../contexts/UserContext";
 
 interface HabitsListProps {
     setHabitToEdit: HabitSetter
@@ -11,20 +13,39 @@ interface HabitsListProps {
 }
 
 function HabitsList({setHabitToEdit, openEdit}: HabitsListProps) {
-    const { habits } = useContext(HabitsContext) as HabitsContextType;
+    const { habits, setHabits } = useContext(HabitsContext) as HabitsContextType;
+    const { currentUser } = useUserContext()
     const [showEdit, setShowEdit] = useState(false)
     const [showTodayOnly, setShowTodayOnly] = useState(false)
 
+    useEffect(() => {
+        getHabits(currentUser)
+        .then((habits) => {
+            setHabits(habits.map(habit => {
+                return {
+                    id: habit.habit_id,
+                    name: habit.habit_name,
+                    description: habit.description,
+                    category: habit.habit_category,
+                    occurrence: habit.occurrence,
+                    completed: false
+                }
+            })) 
+        })
+        .catch((err) => {
+            console.log("ERROR");
+        })
+    }, [])
 
     const filteredHabits = showTodayOnly
     ? habits.filter((habit) => habit.occurrence.includes(new Intl.DateTimeFormat("en-US", { weekday: "long"}).format(new Date()))) 
     : habits;
 
     return (
-        <View>
+        <View style={{flexBasis: 0, flexGrow: 1}}>
             <Button onPress={() => setShowTodayOnly(!showTodayOnly)}>
             {showTodayOnly ? "Show All Habits" : "Show Today's Habits"}
-          </Button>
+            </Button>
             <FlatList
                 data={filteredHabits}
                 keyExtractor={(item) => item.id.toString()}
